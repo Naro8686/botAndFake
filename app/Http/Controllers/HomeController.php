b060ac52fdd2569d3441e2976bce0f62ad7ef171
Narek Melqonyan <narek199121@gmail.com>
2022-01-19 16:33:32 +0400

    small change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\SendEmailFake;
-use App\Models\Category;
-use App\Models\Fake;
-
 class HomeController extends Controller
 {
     /**
@@ -15,11 +11,6 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $fake = Fake::whereHas('category',function ($query){
-            $query->where('name',Category::VINTED);
-        })->first();
-        \Mail::to('dasdad@dasd.ru')->send(new SendEmailFake($fake));
-        dd($fake);
         $this->middleware('auth');
     }
 
